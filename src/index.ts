8eaf34e550b89f3fdcd8728209e181928868646d
Henning Berge <henning.ber@gmail.com>
2022-05-05 09:41:37 +0200

    optionally submit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
-import md5File from 'md5-file';
 import path from 'path';
 import admin from 'firebase-admin';
 import { FirebaseCompare } from './firebase-compare';
@@ -16,12 +15,9 @@ admin.initializeApp({
 async function main(functions_dir: string, should_publish: boolean) {
     const FUNCTIONS_PATH: string = path.resolve('./', functions_dir);
     const seperatedFunctions = TypescriptParse('src/index.ts', FUNCTIONS_PATH);
-    const createdFiles = await Packer(seperatedFunctions, FUNCTIONS_PATH);
+    const functionHashes = await Packer(seperatedFunctions, FUNCTIONS_PATH);
     //
-    const functionHashes: [string, string][] = Object.keys(createdFiles).map((name) => [
-        name,
-        md5File.sync(path.resolve(FUNCTIONS_PATH, 'dist', name, 'index.js')),
-    ]);
+
     const changes = await FirebaseCompare(Object.fromEntries(functionHashes));
     if (should_publish) {
         await firebaseDeploy(changes);
--- src/packer.ts
+++ src/packer.ts
@@ -1,54 +1,72 @@
 import path from 'path';
 import VirtualModulesPlugin from 'webpack-virtual-modules';
 import webpack from 'webpack';
+import md5File from 'md5-file';
 
-export function Packer(
+export async function Packer(
     content: { name: string; template: string }[],
     baseDir: string
-): Promise<Record<string, string>> {
-    const modules: Record<string, string> = {};
-    const entries: Record<string, string> = {};
-    content.forEach((func) => {
-        modules[path.resolve(baseDir, 'src', `func${func.name}.ts`)] = func.template;
-        entries[func.name] = path.resolve(baseDir, 'src', `func${func.name}.ts`);
-    });
-    const virtualModules = new VirtualModulesPlugin(modules);
-    return new Promise((resolve, reject) => {
-        webpack(
-            {
-                mode: 'production',
-                context: baseDir,
-                entry: entries,
-                module: {
-                    rules: [
-                        {
-                            test: /\.tsx?$/,
-                            use: 'ts-loader',
-                            exclude: /node_modules/,
-                        },
-                    ],
-                },
-                plugins: [virtualModules],
-                target: 'node',
-                resolve: {
-                    extensions: ['.tsx', '.ts', '.js'],
-                    modules: [path.resolve(__dirname, '..', 'node_modules'), 'node_modules'],
-                },
-                output: {
-                    filename: '[name]/index.js',
-                    path: path.resolve(baseDir, 'dist'),
-                },
-            },
-            (err, stats) => {
-                // [Stats Object](#stats-object)
-                if (err || stats.hasErrors()) {
-                    // [Handle errors here](#error-handling)
-                    console.error(err || stats.compilation.errors);
-                    return reject(err || stats.compilation.errors);
-                }
-                // Done processing
-                return resolve(entries);
+): Promise<[string, string][]> {
+    const functionHashes: [string, string][] = [];
+    while (content.length > 0) {
+        const modules: Record<string, string> = {};
+        const entries: Record<string, string> = {};
+
+        for (let i = 0; i <= 10; i++) {
+            if (i >= 10 || content.length === 0) {
+                break;
             }
+            const func = content.pop();
+            modules[path.resolve(baseDir, 'src', `func${func.name}.ts`)] = func.template;
+            entries[func.name] = path.resolve(baseDir, 'src', `func${func.name}.ts`);
+        }
+        const virtualModules = new VirtualModulesPlugin(modules);
+        // eslint-disable-next-line no-await-in-loop
+        const createdFiles = await new Promise((resolve, reject) => {
+            webpack(
+                {
+                    mode: 'production',
+                    context: baseDir,
+                    entry: entries,
+                    module: {
+                        rules: [
+                            {
+                                test: /\.tsx?$/,
+                                loader: 'ts-loader',
+                                options: { allowTsInNodeModules: true },
+                            },
+                        ],
+                    },
+                    plugins: [virtualModules],
+                    target: 'node',
+                    resolve: {
+                        extensions: ['.tsx', '.ts', '.js'],
+                        modules: ['node_modules/firebase-functions-webpack/node_modules', 'node_modules'],
+                    },
+                    output: {
+                        filename: '[name]/index.js',
+                        path: path.resolve(baseDir, 'dist'),
+                    },
+                    externals: {
+                        sharp: 'commonjs sharp',
+                    },
+                },
+                (err, stats) => {
+                    // [Stats Object](#stats-object)
+                    if (err || stats.hasErrors()) {
+                        // [Handle errors here](#error-handling)
+                        console.error(err || stats.compilation.errors);
+                        return reject(err || stats.compilation.errors);
+                    }
+                    // Done processing
+                    return resolve(entries);
+                }
+            );
+        });
+
+        Object.keys(createdFiles).forEach((name) =>
+            functionHashes.push([name, md5File.sync(path.resolve(baseDir, 'dist', name, 'index.js'))])
         );
-    });
+    }
+    return functionHashes;
 }
